d4b562827b8c53a6fd6c98630637baa615640bdd
Le Hieu <lehieu013@gmail.com>
2021-07-02 11:14:38 +0700

    Update

--- setup.sh
+++ setup.sh
@@ -3,12 +3,15 @@
 # Install neovim
 sudo apt install neovim;
 # Add alias
-[ `alias | grep nvim | wc -l` != 0 ] && echo 'alias v=nvim' >> ~/.bashrc && . ~/.bashrc;
+if [ `alias | grep nvim | wc -l` != 0 ] 
+then
+    echo 'alias v=nvim' >> ~/.bashrc && . ~/.bashrc;
+fi
 # Install vim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim';
 # copy vc
 cp init.vim ~/.config/nvim/;
 # setup plugin
-v -c 'PluginInstall';
+v -c 'PlugInstall';
 
